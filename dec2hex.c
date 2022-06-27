26b5951360d2e44ffc00201efbba4cddfe76f293
rasika511 <rasika.bhave5@gmail.com>
2022-06-27 15:31:18 -0700

    Initialize Starter code for Dec2Hex

---
+++ dec2hex.c
@@ -0,0 +1,25 @@
+/**
+ * Assignment-1
+ * CSE30 Username: cs30s122 (fill out your username)
+ * Class: UCSD CSE30-S122
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[]) {
+
+
+    if (argc != 2) {
+        fprintf(stderr, "%s wrong number of arguments\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+
+    unsigned int population = (unsigned) atoi(argv[1]);
+  
+    // put your code here (use as many lines as you need)
+
+
+    return EXIT_SUCCESS; 
+}
