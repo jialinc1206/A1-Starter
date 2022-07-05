9ed527ba4821c5fcf9818b6357f6f896734bf82d
Aaron Chen <36271006+jialinc1206@users.noreply.github.com>
2022-07-05 09:52:56 -0700

    pa1

--- dec2hex.c
+++ dec2hex.c
@@ -19,6 +19,27 @@ int main(int argc, char* argv[]) {
     unsigned int base10 = (unsigned) atoi(argv[1]);
   
     // put your code here (use as many lines as you need)
+    if(base10 == 0)
+        return EXIT_SUCCESS; 
+    //printf("argv[1] = %d \n", base10);
+    int mod = 0, power = 0;
+    char hexMod;
+
+    while(base10 != 0) {
+       // printf("base10 != 0, = %d \n", base10);
+        mod = base10%16;
+        //printf("mod = %d \n", mod);
+        base10 = base10/16;
+        //printf("base10/16 = %d \n", base10);
+        
+        if(mod < 10)
+            hexMod = mod + '0';
+        else
+            hexMod = mod + 'a' - 10;
+
+        printf("%c * 16^%d \n", hexMod, power);
+        power++;
+    }
 
 
     return EXIT_SUCCESS; 
